2da97b761c8992772afe8c218e0a984c0bfcfc80
MylesUnlimited <75455763+MylesUnlimited@users.noreply.github.com>
2023-04-25 21:39:52 -0400

    Add files via upload

--- udp-client.py
+++ udp-client.py
@@ -1,23 +1,147 @@
 import socket
 import random as r
+import os.path as path
+import sys
 
 
-server_address = '192.168.1.155'
+server_address = '192.168.1.152'
 port = 13000
 X = (r.randint(1,10000))
+
 name = "peer"+ str(X)
 
+'''
+def send_file( address: (str, int),filename: str):
+    # get the file size in bytes
+    # TODO: section 2 step 2
+    file_size = get_file_size(filename)
+    # convert file_size to an 8-byte byte string using big endian
+    # TODO: section 2 step 3
+    size = (file_size).to_bytes(8, byteorder='big')
+    name = filename.encode()
+
+    # create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # TODO: section 2 step 5
+        client_socket.connect((IP,PORT))
+        # send the file size in the first 8-bytes followed by the bytes
+        # for the file name to server at (IP, PORT)
+        # TODO: section 2 step 6
+        message = size + name
+        # TODO: section 2 step 7
+        client_socket.sendto(message, (IP, PORT))
+        reply = client_socket.recv(BUFFER_SIZE)
+        if reply != b'go ahead':
+            raise Exception('Bad server response - was not go ahead!')
+
+        # open the file to be transferred
+        with open(file_name, 'rb') as file:
+            # read the file in chunks and send each chunk to the server
+            is_done = False
+            while not is_done:
+                chunk = file.read(BUFFER_SIZE)
+                if len(chunk) > 0:
+                    client_socket.send(chunk)
+                elif len(chunk) == 0:
+                    is_done = True
+
+                # TODO: section 2 step 8a
+                # TODO: section 2 step 8b
+    except OSError as e:
+        print(f'An error occurred while sending the file:\n\t{e}')
+    finally:
+        client_socket.close()
+
+
+#def request_file(filename, address):
+
+
+
+def upload_file(conn_socket: socket, file_name: str, file_size: int):
+    # create a new file to store the received data
+    file_name = 'ad-Image'
+    open('ImageWasModified', 'w')
+    # please do not change the above line!
+
+    with open(file_name, 'wb') as file:
+        retrieved_size = 0
+        try:
+            while retrieved_size < file_size:
+                # TODO: section 1 step 6a
+                chunk = conn_socket.recv(BUFFER_SIZE)
+                retrieved_size += len(chunk)
+                file.write(chunk)
+                # TODO: section 1 stop 6b
+                # TODO: section 1 stop 6c
+        except OSError as oe:
+            print(oe)
+            os.remove(file_name)
+
+
+'''
+
+def get_file_size(file_name: str) -> int:
+    size = 0
+    try:
+        size = path.getsize(file_name)
+    except FileNotFoundError as fnfe:
+        print(fnfe)
+        sys.exit(1)
+    return size
+
+def request_file(filename):
+    file = bytes(filename.encode())
+    udp_client_socket.sendto(file,(server_address,port))
+
+    response,address = udp_client_socket.recvfrom(1024)
+
+def upload_file()
+
+def join_Network(name):
+
+    udp_client_socket.sendto(b"J" + name.encode(), (server_address, port))
+    response, address = udp_client_socket.recvfrom(1024)
+
+    if response[8:] == b"peer joined":
+        print("Peer Joined")
+        return int.from_bytes(response[:8],byteorder='big')
+
+
 if __name__ == "__main__":
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hostname = socket.gethostname()
+
     try:
         while True:
-            message = input("What's your message: ")
-            message_type = b'R'
-            client_socket.sendto(message_type + name.encode(), (server_address, port))
-            response, address = client_socket.recvfrom(1024)
-            print(f'Response from {address[0],address[1]} is "{response.decode()}"')
+            message = input("Message Type: ")
+            message_type = bytes(message.encode())
+
+            if message == "J":
+                tcp_port = join_Network(name)
+                print(tcp_port)
+                server_socket.bind((socket.gethostbyname(hostname), tcp_port))
+
+            if message == "U":
+                filename = input("Filename: ")
+                size = (get_file_size(filename)).to_bytes(8,byteorder='big')
+                udp_client_socket.sendto(size + bytes(filename.encode()),(server_address,port))
+
+            '''
+            if response == b"request":
+                client_socket.send(b"go ahead")
+                file, address = client_socket.recvfrom(1024)
+                send_file( file,address)
+            '''
+
+            #print(f'Response from {address[0],address[1]} is "{response.decode()}"')
     except KeyboardInterrupt as ki:
         print("Shutting down...")
     finally:
 
-        client_socket.close()
+        udp_client_socket.close()
+        tcp_client_socket.close()
+        server_socket.close()
--- udp-server.py
+++ udp-server.py
@@ -2,35 +2,55 @@ import socket
 import random as rand
 import pandas as pd
 
-my_address = '192.168.1.155'
+my_address = '192.168.1.152'
 port = 13000
-f = open("test.txt","r+")
-t = f.read()
-if ("Peer_Name,Peer_Address\n") not in t:
-    f.write("Peer_Name,Peer_Address\n")
-    f.close()
-else:
-    f.close()
+
+peers = {"peer423":('127.0.0.1', 59755)}
+
+files = {("test.png",25):"peer423"}
+
+
+def peer_join(name, address):
+    peers[name] = address
+    print((address[1]))
+
+    portnum = (address[1]).to_bytes(8, byteorder='big')
+
+    server_socket.sendto(portnum+b"peer joined",(address[0],address[1]))
+
+    print(f"{name} has joined")
+    print(peers)
+
+def get_file_info(data: bytes) -> (str, int):
+    return data[8:].decode(), int.from_bytes(data[:8], byteorder='big')
+
+def upload_file(name, filename, filesize):
+    files[(filename,filesize)] = name
+
 if __name__ == "__main__":
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((my_address, port))
     print(f"Server is ready on {my_address}:{port}")
     try:
         while True:
-            f = open("test.txt","a")
             message, client_address = server_socket.recvfrom(1024)
             message_type = message[:1]
-            peer_name = message[1:].decode()
+
+            if message_type == b"J":
+                peer_name = message[1:].decode()
+                peer_join(peer_name,client_address)
+
+            if message_type == b"U":
+
+
+            #send client address back to client
+
             #ize = int.from_bytes(size, byteorder='big')
-            print(f'message type = {message_type} and peer = {peer_name}')
             # message, client_address = server_socket.recvfrom(size)
             #response = ''.join(list(map(lambda ch: '' if ch in 'aeiou' else ch, message)))
-            response = peer_name
-            f.write(peer_name+','+str(client_address)+'\n')
-            f.close()
-            server_socket.sendto(response.encode(), client_address)
     except KeyboardInterrupt as ki:
         print("Shutting down...")
     finally:
 
         server_socket.close()
+
