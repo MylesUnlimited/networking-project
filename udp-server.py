aff45ae9e112307cad282009ffeddd8d0cb841bb
javauniedowns <downsjavaunie@gmail.com>
2023-05-02 00:09:41 -0400

    done

--- udp-server.py
+++ udp-server.py
@@ -7,18 +7,6 @@ peers = {"peer423":('127.0.0.1', 59755)}
 
 files = {("test.png",25):"peer423"}
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-
 def peer_join(name, address, tAddress):
     peers[name] = tAddress
     print((address[1]))
